a35675b7fe163ad264faf0c4a8009facf37d85e5
Cuong Nguyen <galvinnguyen1999@gmail.com>
2025-06-29 21:33:50 +0700

    feat: sign up user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
-import { userApi } from "@/lib/api";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -14,18 +13,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET as string,
-  callbacks: {
-    async signIn({ user }) {
-      await userApi.create({
-        email: user.email!,
-        name: user.name || "",
-        avatar: user.image || "",
-      });
-
-      return true;
-    },
-  },
+  secret: process.env.NEXTAUTH_SECRET as string
 };
 
 export default NextAuth(authOptions);
