71cd9e72a7c54f6b17cd5d4da8b62f52387f8fc9
Cuong Nguyen <galvinnguyen1999@gmail.com>
2025-06-29 16:34:21 +0700

    refactor: update home page to handle authentication and redirect users based on session status

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
-  return (
-    <div>
-      Test
-    </div>
-  );
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/vi/dashboard");
+  } else {
+    redirect("/vi/login");
+  }
 }
