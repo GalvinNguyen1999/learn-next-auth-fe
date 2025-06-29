3418170f926340262e533a37ab6fd1ea2894d4d6
Cuong Nguyen <galvinnguyen1999@gmail.com>
2025-06-29 12:14:46 +0700

    feat: save user in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -9,6 +9,23 @@ export const authOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET as string,
+  callbacks: {
+    async signIn({ user }: { user: any }) {
+      console.log("signIn", user);
+
+      await fetch("http://localhost:4000/user/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: user.email,
+          name: user.name,
+          avatar: user.image,
+        }),
+      });
+
+      return true;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
